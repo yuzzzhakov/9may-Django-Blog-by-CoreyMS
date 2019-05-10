fea435ecba895cb3e3f58be78278af980bf4c4ca
yuzzzhakov <yuzzzhakaov@ya.ru>
2019-05-11 00:39:19 +0300

    11.05 0.39 The end of Part 5

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,25 +1,11 @@
 from django.shortcuts import render
-
-
-posts = [
-    {
-        'author': 'CoreyMS',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2018',
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 28, 2018',
-    }
-]
+from .models import Post
+from django.contrib.auth.models import User
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
