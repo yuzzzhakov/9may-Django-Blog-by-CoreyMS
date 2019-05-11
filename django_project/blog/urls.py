86f8ed48bd064cc54394cda6f48af3a3dc9320ef
yuzzzhakov <yuzzzhakaov@ya.ru>
2019-05-12 00:45:46 +0300

    12.05 0.45 The end of Part 11

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -5,11 +5,13 @@ from .views import (
     PostCreateView,
     PostUpdateView,
     PostDeleteView,
+    UserPostListView,
 )
 from . import views
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
+    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'), #post_form.html
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'), #post_form.html
